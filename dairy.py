85ae6c76de8e35c2d54599dcb6fbbec97c421bc8
Yotsuba <jipalaoag2228@gmail.com>
2026-07-17 20:40:43 +0800

    cleaned code and bug fixes

--- dairy.py
+++ dairy.py
@@ -10,25 +10,28 @@ def main():
     date = datetime.now()
     print(f"{date.strftime("%A, %B %d, %Y")}")
     print(f"{date.strftime("%Y-%m-%d %H:%M:%S")}")
-    choice = input("""
+    print("""
          [1] Write a new entry
          [2] Read past entries 
          [3] Exit 
                     """)
     while True: 
-         choice = input("What would you like to do?: ").strip()
-         if choice == 1:
+         choice = input("What would you like to do?: ")
+         if choice == "1":
             writenew(date)
-         if choice == 2:
+            break
+         elif choice == "2":
             pass
-         if choice == 3:
+         elif choice == "3":
             sys.exit()
+         else:
+             print("Invalid input.")
 
 def writenew(date):
-    filename = date.strftime("%m-%d %H:%M")
+    filename = date.strftime("%m-%d, %Hh%Mm%Ss")
     filepath = pathlib.Path("log")/filename
-    with open(f"{filepath}", "a") as file:
+    with open(filepath, "a") as file:
         entry = input("Start writing here: ")
-        file.write(f"{entry}")
+        file.write(f"{entry}")  
 
 main()
