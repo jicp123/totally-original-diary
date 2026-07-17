9e1025101bc430756e7cba27ca0b6736e6368d2c
Yotsuba <jipalaoag2228@gmail.com>
2026-07-17 11:00:15 +0800

    added entry func

--- dairy.py
+++ dairy.py
@@ -9,6 +9,7 @@ import sys
 def main():
     date = datetime.now()
     print(f"{date.strftime("%A, %B %d, %Y")}")
+    print(f"{date.strftime("%Y-%m-%d %H:%M:%S")}")
     choice = input("""
          [1] Write a new entry
          [2] Read past entries 
@@ -17,10 +18,17 @@ def main():
     while True: 
          choice = input("What would you like to do?: ").strip()
          if choice == 1:
-            pass
+            writenew(date)
          if choice == 2:
             pass
          if choice == 3:
             sys.exit()
+
+def writenew(date):
+    filename = date.strftime("%m-%d %H:%M")
+    filepath = pathlib.Path("log")/filename
+    with open(f"{filepath}", "a") as file:
+        entry = input("Start writing here: ")
+        file.write(f"{entry}")
 
 main()
